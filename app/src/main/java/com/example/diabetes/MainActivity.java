b01409853f79e43742809d012d9152903b25dddf
Dzenan Becirovic <63358865+maglichito@users.noreply.github.com>
2021-07-07 00:20:16 +0200

    Diabetes+

---
+++ app/src/main/java/com/example/diabetes/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.diabetes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+    }
+}
