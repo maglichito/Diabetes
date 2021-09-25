2299ce19cbd538dc747991a6f57ac2dad3f1c3e6
Dzenan Becirovic <63358865+maglichito@users.noreply.github.com>
2021-09-26 01:23:38 +0200

    Transition

--- app/src/main/java/com/example/diabetes/Fragment1_Register.java
+++ app/src/main/java/com/example/diabetes/Fragment1_Register.java
@@ -2,11 +2,16 @@ package com.example.diabetes;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Fragment1_Register extends Fragment {
@@ -19,5 +24,26 @@ public class Fragment1_Register extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment1__register, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button register_button = getActivity().findViewById(R.id.get_started);
+        register_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                Fragment2_Register fragment2 = new Fragment2_Register();
+                manager.beginTransaction()
+                        .setCustomAnimations(R.anim.enter_left_to_right,R.anim.exit_left_to_right)
+                        .replace(R.id.frame_layout,fragment2)
+                        .addToBackStack(null)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+        });
     }
 }
